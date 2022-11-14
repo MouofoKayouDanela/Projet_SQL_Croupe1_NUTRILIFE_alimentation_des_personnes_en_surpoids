af4cb95da4bbc63704f2c32fee39a615a8cad4b4
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2022-11-14 03:12:58 +0100

    Création du dossier d'insertion de données et insertion des données dans la table personne

---
+++ Donnees/DoPersonne.sql
@@ -0,0 +1,336 @@
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEOO1',
+    'Elessa',
+    'Goerges',
+    TO_DATE('17/09/1983', 'dd/mm/yyyy'),
+    'masculin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEOO2',
+    'Anguissa',
+    'Samuel',
+    TO_DATE('30/05/1989', 'dd/mm/yyyy'),
+    'masculin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEOO3',
+    'Ewalle',
+    'Clemantine',
+    TO_DATE('22/02/1982', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEOO4',
+    'Kwelle',
+    'Augustin',
+    TO_DATE('01/09/1990', 'dd/mm/yyyy'),
+    'masculin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEOO5',
+    'Malla',
+    'Celestine',
+    TO_DATE('19/03/1986', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEOO6',
+    'Ekollo',
+    'Ernestine',
+    TO_DATE('07/09/1988', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEOO7',
+    'Nembot',
+    'Appolite',
+    TO_DATE('12/08/1984', 'dd/mm/yyyy'),
+    'masculin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEOO8',
+    'Temgoua',
+    'Marcel',
+    TO_DATE('21/12/1991', 'dd/mm/yyyy'),
+    'masculin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEOO9',
+    'Siyapze',
+    'Nicole',
+    TO_DATE('06/10/1992', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEO10',
+    'Ekalle',
+    'Vincent',
+    TO_DATE('15/04/1982', 'dd/mm/yyyy'),
+    'masculin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEOO1',
+    'Kameni',
+    'Sidonie',
+    TO_DATE('31/07/1987', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEO11',
+    'Kenfack',
+    'Nadege',
+    TO_DATE('12/11/1988', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEO12',
+    'Kemdeng',
+    'Virginie',
+    TO_DATE('29/06/1992', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEO13',
+    'Maimouna',
+    'Saadiya',
+    TO_DATE('11/10/1990', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEO14',
+    'Ngueguaing',
+    'Rose',
+    TO_DATE('02/05/1986', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEO15',
+    'Etonde',
+    'Geraldine',
+    TO_DATE('14/02/1987', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEO16',
+    'Biwalle',
+    'Armandine',
+    TO_DATE('07/08/1985', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PE017',
+    'Womecha',
+    'Marie',
+    TO_DATE('15/01/1982', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEO18',
+    'Poumeni',
+    'Philomene',
+    TO_DATE('08/08/1988', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEO19',
+    'Tchoumeni',
+    'Esthere',
+    TO_DATE('16/05/1991', 'dd/mm/yyyy'),
+    'feminin'
+);
+INSERT INTO PERSONNE
+(
+    id_personne,
+    nom_personne,
+    prenom_personne,
+    date_naissance,
+    genre
+)
+VALUES
+(
+    'PEO20',
+    'Essengue',
+    'Paul',
+    TO_DATE('27/06/1983', 'dd/mm/yyyy'),
+    'masculin'
+);
