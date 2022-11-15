aca582f54b4d215656eb3019c9ff927bee7d85ea
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2022-11-15 08:56:24 +0100

    Appel de tous les scripts d'ajout de contraintes de valeurs obligatoires

---
+++ Tables/Contraintes/ValeursNulles/VnIndex.sql
@@ -0,0 +1,12 @@
+PROMPT ****** contraintes de valeurs obligatoires ******
+@@VnActivites
+@@VnIngredient_repas
+@@VnIngredient
+@@VnParametresMembres
+@@VnPersonne
+@@VnProgramme_regime
+@@VnRegime_repas
+@@VnRegimeActivite
+@@VnRepas
+@@VnSouscription
+@@VnUtilisateur 
