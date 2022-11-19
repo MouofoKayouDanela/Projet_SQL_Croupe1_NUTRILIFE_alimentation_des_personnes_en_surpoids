0da4daa801c5553a6ea30b1803e07d090ce1464c
Mouofo Kayou Danela <107385969+danelamkdap@users.noreply.github.com>
2022-11-19 09:27:49 +0100

    MODIFICATION DES NOMS DES VARIABLES DES FONCTIONNALITES

--- Objets/Fonctionnalites/Ajouter/FoAjouterRepasMenu.sql
+++ Objets/Fonctionnalites/Ajouter/FoAjouterRepasMenu.sql
@@ -1,4 +1,11 @@
 
-INSERT INTO REGIME_REPAS(id_programme_regime,id_repas,jour_repas,periode,type_repas)
-VALUES(&ID,(SELECT id_repas FROM REPAS WHERE id_repas=&id_r),'&jour',&periode,'&type_repas' );
+INSERT INTO
+ REGIME_REPAS
+ (id_programme_regime,id_repas,jour_repas,periode,type_repas)
+VALUES
+(&ID,
+(SELECT id_repas FROM REPAS WHERE id_repas=&id_repas_a_ajouter),
+'&jour',
+&periode,
+'&type_repas' );
 
