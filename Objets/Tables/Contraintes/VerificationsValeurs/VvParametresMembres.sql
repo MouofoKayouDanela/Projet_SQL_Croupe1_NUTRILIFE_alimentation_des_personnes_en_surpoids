9bb1036e46f6fbf3f662d25ff5a8cef8bdc69a78
tedongmo valdez <tedongmokanouovaldez@gmail.com>
2022-11-19 10:52:47 +0100

    modification des contraintes des valeurs des parametres des membres

--- Objets/Tables/Contraintes/VerificationsValeurs/VvParametresMembres.sql
+++ Objets/Tables/Contraintes/VerificationsValeurs/VvParametresMembres.sql
@@ -3,5 +3,6 @@ ADD CONSTRAINT chk_parametres_membres
 CHECK
 (
     masse_initiale > 0 AND 
-    taille_utilisateur > 0 
+    taille_utilisateur > 0 AND 
+    (ROUND((masse_initiale / (taille_utilisateur * taille_utilisateur)), 1) BETWEEN 25 AND 29.9)
 );
