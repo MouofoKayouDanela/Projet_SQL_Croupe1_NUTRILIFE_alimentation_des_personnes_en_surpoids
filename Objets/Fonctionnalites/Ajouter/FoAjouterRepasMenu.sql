1a1301282cbea719c8986f98645e5b9d9eaaf5b4
tedongmo valdez <tedongmokanouovaldez@gmail.com>
2022-11-19 14:30:39 +0100

    modification de fonctionnalites

--- Objets/Fonctionnalites/Ajouter/FoAjouterRepasMenu.sql
+++ Objets/Fonctionnalites/Ajouter/FoAjouterRepasMenu.sql
@@ -4,7 +4,7 @@ INSERT INTO
  (id_programme_regime,id_repas,jour_repas,periode,type_repas)
 VALUES
 (&ID,
-(SELECT id_repas FROM REPAS WHERE id_repas=&id_repas_a_ajouter),
+(SELECT id_repas FROM REPAS WHERE nom_repas='&nom_repas_a_ajouter'),
 '&jour',
 &periode,
 '&type_repas' );
